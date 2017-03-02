adecfb8b510d1efe3bad8acd8be314c1fcc99b97
lokesh <lokesh@XSILOKESH>
2017-03-02 11:29:12 +0530

    latest

--- Inventory.Repository/Inventory.Repository/WarehouseRepository.cs
+++ Inventory.Repository/Inventory.Repository/WarehouseRepository.cs
@@ -23,9 +23,13 @@ namespace Inventory.Repository
             return count;
         }
 
-        public static int WHaddressinsert(string WH_Name,string WH_ShortName,string contactperson,string jobposition,int phone,int mobile,string Email,string Note,string bill_street,string bill_city,string bill_state,string bill_postalcode,string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country)
+        public static int WHaddressinsert(string WH_Name,string WH_ShortName,string contactperson,string jobposition,
+            int phone, int mobile,string Email,string Note,string bill_street,string bill_city,string bill_state,string bill_postalcode,
+            string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country)
         {
-            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "insertwarehouse",WH_Name,WH_ShortName,contactperson, jobposition, phone,mobile, Email,Note,bill_street,bill_city,bill_state,bill_postalcode,bill_country,ship_street,ship_city,ship_state,ship_postalcode,ship_country);
+            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "insertwarehouse",WH_Name,WH_ShortName,contactperson, jobposition, phone,mobile, 
+                Email,Note,bill_street,
+                bill_city,bill_state,bill_postalcode,bill_country,ship_street,ship_city,ship_state,ship_postalcode,ship_country);
             return count;
         }
     }
--- Inventory/Controllers/WarehouseController.cs
+++ Inventory/Controllers/WarehouseController.cs
@@ -24,17 +24,18 @@ namespace Inventory.Controllers
         {
             return View();
         }
-        //public ActionResult WarehouseAddress()
-        //{
-        //    return View();
-        //}
+
         [HttpPost]
         public ActionResult Index(Warehouse wh)
         {
 
-            int count = WHservice.WHaddressinsert(wh.wh_name, wh.wh_Shortname, wh.conperson, wh.Job_position, wh.phone, wh.Mobile, wh.Email, wh.Note, wh.bill_Street, wh.bill_City, wh.bill_State, wh.bill_Postalcode, wh.bill_Country, wh.ship_Street, wh.ship_City, wh.ship_State, wh.ship_Postalcode, wh.ship_Country);
+            int count = WHservice.WHaddressinsert(wh.wh_name, wh.wh_Shortname, wh.conperson, wh.Job_position,
+                wh.phone, wh.Mobile, wh.Email, wh.Note, wh.bill_Street, wh.bill_City, wh.bill_State, wh.bill_Postalcode, wh.bill_Country,
+                wh.ship_Street, wh.ship_City, wh.ship_State, wh.ship_Postalcode, wh.ship_Country);
             Response.Write("<script language='javascript' type='text/javascript'>alert('Warehouse Registration successful.') </script>");
+
             return View();
+
         }
     }
 }
