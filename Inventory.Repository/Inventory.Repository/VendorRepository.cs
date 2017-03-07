7bbf94c83ffb364ad1cdd06f0626d771ccc8f1d4
sravani <sravani.siddeswara@xsilica.com>
2017-03-07 11:59:46 +0530

    latest

--- Inventory.Repository/Inventory.Repository/VendorRepository.cs
+++ Inventory.Repository/Inventory.Repository/VendorRepository.cs
@@ -1,40 +1,59 @@
-﻿using Microsoft.ApplicationBlocks.Data;
-using System;
-using System.Collections.Generic;
+﻿using Inventory.Utility;
+using Microsoft.ApplicationBlocks.Data;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Inventory.Repository
 {
   public class VendorRepository
     {
-        
+
         private static string ConnectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ToString();
         private static string ConnectionString1 = ConfigurationManager.ConnectionStrings["DbConnection1"].ToString();
+
+        #region CompanyInsertRow
+
+        public static int CompanyInsertRow(string Company_name,int Bank_Acc_Number,string Bank_Name,string Bank_Branch,int Paytym_Number,string Email,byte[] Logo)
+        {
+            
+            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "insertCompany", Company_name, Bank_Acc_Number, Bank_Name, Bank_Branch, Paytym_Number, Email, Logo);
+            //int id = SqlHelper
+            return count;
+        }
+        #endregion
+
+
         #region VendorInsertRow
 
-        public static int VendorInsertRow(string Contact_Person1Fname, long Mobile_No,
-            long Bank_Acc_Number, long Paytym_Number, string Company_Name, string Contact_Person1Lname, long LandLine_Num,
-            string Bank_Name, string Bank_Branch, byte[] Logo, string Remarks, string Contact_Person2Lname, string Contact_Person2Fname,
-            string Email, string Adhar_Number)
+        public static int VendorInsertRow(string Contact_PersonFname, string Contact_PersonLname, long Mobile_No, long LandLine_Num,
+                         string Remarks, string Email, string Adhar_Number,string Job_position)
         {
            
-            int count= SqlHelper.ExecuteNonQuery(ConnectionString, "insertvendor", Contact_Person1Fname, Mobile_No, Bank_Acc_Number, Paytym_Number, Company_Name, Contact_Person1Lname, LandLine_Num, Bank_Name, Bank_Branch, Logo, Remarks, Contact_Person2Lname, Contact_Person2Fname, Email, Adhar_Number);
+            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "insertvendor", Contact_PersonFname, Contact_PersonLname, Mobile_No, LandLine_Num,Remarks, Email, Adhar_Number, Job_position);
             return count;
         }
         #endregion
         #region VendorAddressInsertRow
-        
-        public static int VendorAddressInsertRow(string Job_position, string Note, string bill_street, string bill_city, string bill_state, string bill_postalcode,
+
+        public static int VendorAddressInsertRow(string bill_street, string bill_city, string bill_state, string bill_postalcode,
             string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country)
         {
 
-            int count1 = SqlHelper.ExecuteNonQuery(ConnectionString, "insertVendor_address", Job_position, Note, bill_street, bill_city, bill_state, bill_postalcode,
+            int count1 = SqlHelper.ExecuteNonQuery(ConnectionString, "insertVendor_address", bill_street, bill_city, bill_state, bill_postalcode,
                 bill_country, ship_street, ship_city, ship_state, ship_postalcode, ship_country);
 
             return count1;
+        }
+        #endregion
+
+        #region UpdateCompany
+        
+        public static int UpdateCompany(int company_Id,string company_name,int Bank_Acc_Number,string Bank_Name,string Bank_Branch,int Paytym_Number,string email,byte[] logo)
+        {
+          
+            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "updateCompany", company_Id, company_name, Bank_Acc_Number, Bank_Name,
+                Bank_Branch, Paytym_Number, email, logo);
+
+            return count;
         }
         #endregion
 
--- Inventory/Controllers/VendorController.cs
+++ Inventory/Controllers/VendorController.cs
@@ -20,48 +20,67 @@ namespace Inventory.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(Vendor vendor, string command)
+        public ActionResult Index(Vendor vendor)
         {
 
             var user = (CustomPrinciple)System.Web.HttpContext.Current.User;//to get loged owner dbname
 
-            int count = VendorService.VendorInsertRow(vendor.Contact_Person1Fname, vendor.Mobile_No, vendor.Bank_Acc_Number,
-                vendor.Paytym_Number, vendor.Company_Name, vendor.Contact_Person1Lname, vendor.LandLine_Num, vendor.Bank_Name, vendor.Bank_Branch, vendor.Logo,
-                vendor.Remarks, vendor.Contact_Person2Lname, vendor.Contact_Person2Fname, vendor.Email, vendor.Adhar_Number);
+            if (Request.Form["submitbutton0"] != null)
+            {
+                int countng = VendorService.CompanyInsertRow(vendor.Company_Name, vendor.Bank_Acc_Number, vendor.Bank_Name, vendor.Bank_Branch,
+                                        vendor.Paytym_Number, vendor.Email, vendor.Logo);
+               
+            }
 
-            string DBName = user.DbName;
-            string PassWord = Membership.GeneratePassword(12, 1);
-            DateTime Created_Date = DateTime.Today;
-            int SubscriptionId = 0;
-            int UserTypeId = 1;
-            string User_Site = null;
-            string[] words = (vendor.Company_Name).Split(' ');
-            string Site = words[0];
-            var data = LoginService.getusersite(Site);
-            if (data.HasRows)
-                User_Site = Site + user.ID; //append owner id to user site 
-            else
-            User_Site = Site;
-            DateTime? SubscriptionDate = null;
-            int IsActive = 0;
-            string Phone = (vendor.Mobile_No).ToString();
-            string activationcode = Guid.NewGuid().ToString();
-            string Profile_Picture = null;
-            string Date_Format = null;
-            string Timezone = null;
-
-            string Currency = null;
-            int counts = LoginService.CreateUser(vendor.Email, vendor.Contact_Person1Fname, vendor.Contact_Person1Lname, DBName, Created_Date, PassWord,
-                  SubscriptionId, UserTypeId, User_Site, vendor.Company_Name, Phone, SubscriptionDate, IsActive, activationcode, Profile_Picture, Date_Format, Timezone, Currency);
-
-
-            Email(vendor.Contact_Person1Fname, vendor.Contact_Person1Lname, vendor.Email, activationcode, PassWord); //Sending Email
-
-            int countin = VendorService.VendorAddressInsertRow(vendor.Job_position, vendor.Note, vendor.bill_street, vendor.bill_city, vendor.bill_state,
+            if (Request.Form["submitbutton1"] != null)
+            {
+                int countin = VendorService.VendorAddressInsertRow(vendor.bill_street, vendor.bill_city, vendor.bill_state,
                 vendor.bill_postalcode, vendor.bill_country, vendor.ship_street, vendor.ship_city, vendor.ship_state, vendor.ship_postalcode, vendor.ship_country);
-            //if(count>0)
-            //return Content("<script language='javascript' type='text/javascript'>alert('Inserted successfully.');location.href='" + @Url.Action("Index", "vendor") + "'</script>"); // Stays in Same View
-            return Content("<script language='javascript' type='text/javascript'>alert('Registration successful. Activation email has been sent to your Email Address.');location.href='" + @Url.Action("Index", "vendor") + "'</script>"); // Stays in Same View                                                                                                                                                                     //createdb();
+            }
+
+            if (Request.Form["submitbutton5"] != null)
+                {
+                int count = VendorService.VendorInsertRow(vendor.Contact_PersonFname, vendor.Contact_PersonLname, vendor.Mobile_No,
+               vendor.LandLine_Num, vendor.Remarks,vendor.emailid, vendor.Adhar_Number, vendor.Job_position);
+              
+            }
+
+            if (Request.Form["submitbutton6"] != null)
+            {
+                int company_Id = vendor.company_Id;
+                int count = VendorService.UpdateCompany(vendor.company_Id,vendor.Company_Name,vendor.Bank_Acc_Number,vendor.Bank_Name,vendor.Bank_Branch,vendor.Paytym_Number,vendor.Email,vendor.Logo);
+            }
+
+
+                //string DBName = user.DbName;
+                //string PassWord = Membership.GeneratePassword(12, 1);
+                //DateTime Created_Date = DateTime.Today;
+                //int SubscriptionId = 0;
+                //int UserTypeId = 1;
+                //string User_Site = null;
+                //string[] words = (vendor.Company_Name).Split(' ');
+                //string Site = words[0];
+                //var data = LoginService.getusersite(Site);
+                //if (data.HasRows)
+                //    User_Site = Site + user.ID; //append owner id to user site 
+                //else
+                //User_Site = Site;
+                //DateTime? SubscriptionDate = null;
+                //int IsActive = 0;
+                //string Phone = (vendor.Mobile_No).ToString();
+                //string activationcode = Guid.NewGuid().ToString();
+                //string Profile_Picture = null;
+                //string Date_Format = null;
+                //string Timezone = null;
+
+                //string Currency = null;
+                //int counts = LoginService.CreateUser(vendor.Email, vendor.Contact_PersonFname, vendor.Contact_PersonLname, DBName, Created_Date, PassWord,
+                //      SubscriptionId, UserTypeId, User_Site, vendor.Company_Name, Phone, SubscriptionDate, IsActive, activationcode, Profile_Picture, Date_Format, Timezone, Currency);
+
+
+                //Email(vendor.Contact_PersonFname, vendor.Contact_PersonLname, vendor.Email, activationcode, PassWord); //Sending Email
+
+                return Content("<script language='javascript' type='text/javascript'>alert('Inserted successfully.');location.href='" + @Url.Action("Index", "vendor") + "'</script>"); // Stays in Same View                                                                                                                                                                     //createdb();
         }
         public void Email(string First_Name, string Last_Name, string EmailId, string activationCode, string PassWord)
         {
