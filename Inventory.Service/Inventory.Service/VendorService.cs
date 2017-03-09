a1df5c5f75289bf86b3add1034f09b6e5b1c5592
sravani <sravani.siddeswara@xsilica.com>
2017-03-09 11:31:44 +0530

    latest

--- Inventory.Service/Inventory.Service/VendorService.cs
+++ Inventory.Service/Inventory.Service/VendorService.cs
@@ -21,45 +21,51 @@ namespace Inventory.Service
             
             return VendorRepository.CompanyInsertRow(Company_name, Email);
         }
-
-
+        
         #endregion
-
-
+        
         #region VendorInsertRow
-        public static int VendorInsertRow(string Contact_PersonFname, string Contact_PersonLname, long Mobile_No, long LandLine_Num,
+        public static int VendorInsertRow(int company_Id,string Contact_PersonFname, string Contact_PersonLname, long Mobile_No, long LandLine_Num,
                          string Remarks, string Email, string Adhar_Number, string Job_position)
         {
-            return VendorRepository.VendorInsertRow(Contact_PersonFname, Contact_PersonLname, Mobile_No, LandLine_Num, Remarks, Email, Adhar_Number, Job_position);
+            return VendorRepository.VendorInsertRow(company_Id,Contact_PersonFname, Contact_PersonLname, Mobile_No, LandLine_Num, Remarks, Email, Adhar_Number, Job_position);
         }
-
-
+        
         #endregion
 
         #region VendorAddressInsertRow
-        public static int VendorAddressInsertRow(string bill_street, string bill_city, string bill_state, string bill_postalcode,
+        public static int VendorAddressInsertRow(string Vendor_Id,string bill_street, string bill_city, string bill_state, string bill_postalcode,
             string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country)
         {
-            return VendorRepository.VendorAddressInsertRow(bill_street, bill_city, bill_state, bill_postalcode,
+            return VendorRepository.VendorAddressInsertRow(Vendor_Id,bill_street, bill_city, bill_state, bill_postalcode,
                 bill_country, ship_street, ship_city, ship_state, ship_postalcode, ship_country);
         }
 
         #endregion
 
         #region UpdateCompany
-        public static int UpdateCompany(int company_Id, string company_name, int Bank_Acc_Number, string Bank_Name, string Bank_Branch, int Paytym_Number, string email, byte[] logo)
+        public static int UpdateCompany(int company_Id,int Bank_Acc_Number, string Bank_Name, string Bank_Branch, int Paytym_Number, string email)
         {
 
-            return VendorRepository.UpdateCompany(company_Id, company_name, Bank_Acc_Number, Bank_Name,
-                Bank_Branch, Paytym_Number, email, logo);
+            return VendorRepository.UpdateCompany(company_Id, Bank_Acc_Number, Bank_Name,Bank_Branch, Paytym_Number, email);
         }
 
         #endregion
-
-        public static SqlDataReader getcompanyId()
+        
+            public static int UpdateNotes(int company_Id, string notes)
         {
 
+            return VendorRepository.UpdateNotes(company_Id, notes);
+        }
+        public static SqlDataReader getcompanyId()
+        {
             return VendorRepository.getcompanyId();
         }
+        public static SqlDataReader getvendorId()
+        {
+            return VendorRepository.getvendorId();
+        }
+      
+
     }
 }
--- Inventory/Controllers/VendorController.cs
+++ Inventory/Controllers/VendorController.cs
@@ -26,10 +26,13 @@ namespace Inventory.Controllers
             vendor = (from DataRow row in dt.Rows
                          select new Vendor()
                          {
+                             company_Id=int.Parse(row["company_Id"].ToString()),
                              Company_Name = row["Company_Name"].ToString(),
                              Email = row["Email"].ToString()
                          }).ToList();
             ViewBag.records = vendor;
+            ViewBag.company_Id = getMaxCompanyID();
+            ViewBag.vendor_Id = getMaxVendorID();
             return View();
             
         }
@@ -38,10 +41,29 @@ namespace Inventory.Controllers
         {
 
             var user = (CustomPrinciple)System.Web.HttpContext.Current.User;//to get loged owner dbname
-
+            if (command=="insertcompany") { 
             int count = VendorService.CompanyInsertRow(vendor.Company_Name, vendor.Email);
-
-            if (count > 0) 
+            }
+            if (command == "insertcontact")
+            {
+                int counts = VendorService.VendorInsertRow(vendor.company_Id, vendor.Contact_PersonFname, vendor.Contact_PersonLname, vendor.Mobile_No, vendor.LandLine_Num, vendor.Remarks,
+                        vendor.Email, vendor.Adhar_Number, vendor.Job_position);
+            }
+            if (command == "insertaddress")
+            {
+                int countng = VendorService.VendorAddressInsertRow(vendor.Vendor_Id, vendor.bill_street, vendor.bill_city, vendor.bill_state, vendor.bill_postalcode,
+                vendor.bill_country, vendor.ship_street, vendor.ship_city, vendor.ship_state, vendor.ship_postalcode, vendor.ship_country);
+            }
+            if (command == "updatecompany")
+            {
+                int county = VendorService.UpdateCompany(vendor.company_Id, vendor.Bank_Acc_Number, vendor.Bank_Name,
+                    vendor.Bank_Branch, vendor.Paytym_Number, vendor.emailid);
+            }
+            if (command == "updatenote")
+            {
+                int countyy = VendorService.UpdateNotes(vendor.company_Id, vendor.Note);
+            }
+           
                 return Content("<script language='javascript' type='text/javascript'>alert('Company Added successfully');location.href='" + @Url.Action("Index", "Vendor") + "'</script>"); // Stays in Same View
                         return Content("<script language='javascript' type='text/javascript'>alert('Failed!!!');location.href='" + @Url.Action("Index", "Vendor") + "'</script>");
         } 
@@ -70,6 +92,27 @@ namespace Inventory.Controllers
 
             }
             return View();
+        }
+        private int getMaxCompanyID()
+        {
+            int company_Id=0;
+            SqlDataReader exec = VendorService.getcompanyId();
+            if (exec.Read())
+            {
+                company_Id = (int)exec["company_Id"];
+                            }
+                       return company_Id;
+        }
+
+        private string getMaxVendorID()
+        {
+            string vendor_Id = null;
+            SqlDataReader exec = VendorService.getvendorId();
+            if (exec.Read())
+            {
+                vendor_Id = exec["vendor_Id"].ToString();
+           }
+                        return vendor_Id;
         }
     }
 }
