d66f3fdc866139518e4c9697ca4304362ce96fc7
lokesh <lokeshkumar@xsilica.com>
2017-03-22 18:15:01 +0530

    Latest

--- Inventory/Controllers/WarehouseController.cs
+++ Inventory/Controllers/WarehouseController.cs
@@ -21,7 +21,7 @@ namespace Inventory.Controllers
     {
         // GET: Warehouse
         Warehouse wh = new Warehouse();
-        public ActionResult Index()
+        public ActionResult Index(string status)
         {
 
             var user1 = (CustomPrinciple)System.Web.HttpContext.Current.User;
@@ -41,20 +41,28 @@ namespace Inventory.Controllers
             ViewBag.records = warehouse;
            ViewBag.wh_id = getMaxwhid();
             ViewBag.contact = getcontactdetails();
+            var wh = getlastinsertedwarehouse(ViewBag.wh_id);
+            if(status == "complete")
+            {
+                ViewBag.Warehouse = 1;
+                ViewBag.wh_name = wh.wh_name;
+                ViewBag.wh_sname = wh.wh_Shortname;
+            }
+
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Index(Warehouse wh)
-        {
-            var user = (CustomPrinciple)System.Web.HttpContext.Current.User;
-            int count = WHservice.WHaddressinsert(wh.wh_name, wh.wh_Shortname, wh.conperson, wh.Job_position,
-                wh.phone, wh.Mobile, wh.Email, wh.Note, wh.bill_Street, wh.bill_City, wh.bill_State, wh.bill_Postalcode, wh.bill_Country,
-                wh.ship_Street, wh.ship_City, wh.ship_State, wh.ship_Postalcode, wh.ship_Country, user.DbName);
-            if (count > 0)
-                return Content("<script language='javascript' type='text/javascript'>alert('Warehouse Added successfully');location.href='" + @Url.Action("Index", "Warehouse") + "'</script>"); // Stays in Same View
-            return Content("<script language='javascript' type='text/javascript'>alert('Failed!!!');location.href='" + @Url.Action("Index", "Warehouse") + "'</script>"); // Stays in Same View
-        }
+        //[HttpPost]
+        //public ActionResult Index(Warehouse wh)
+        //{
+        //    var user = (CustomPrinciple)System.Web.HttpContext.Current.User;
+        //    int count = WHservice.WHaddressinsert(wh.wh_name, wh.wh_Shortname, wh.conperson, wh.Job_position,
+        //        wh.phone, wh.Mobile, wh.Email, wh.Note, wh.bill_Street, wh.bill_City, wh.bill_State, wh.bill_Postalcode, wh.bill_Country,
+        //        wh.ship_Street, wh.ship_City, wh.ship_State, wh.ship_Postalcode, wh.ship_Country, user.DbName);
+        //    if (count > 0)
+        //        return Content("<script language='javascript' type='text/javascript'>alert('Warehouse Added successfully');location.href='" + @Url.Action("Index", "Warehouse") + "'</script>"); // Stays in Same View
+        //    return Content("<script language='javascript' type='text/javascript'>alert('Failed!!!');location.href='" + @Url.Action("Index", "Warehouse") + "'</script>"); // Stays in Same View
+        //}
 
         private string getMaxwhid()
         {
@@ -67,7 +75,25 @@ namespace Inventory.Controllers
             }
             return wh_id;
         }
+        private Warehouse getlastinsertedwarehouse(string wh_id)
+        {
+            
+            var user1 = (CustomPrinciple)System.Web.HttpContext.Current.User;
+            SqlDataReader value = WHservice.getlastinsertedwarehouse(user1.DbName, wh_id);
+            DataTable dt = new DataTable();
+            dt.Load(value);
+            Warehouse wh = new Warehouse();
+            wh = (from DataRow row in dt.Rows
+                      select new Warehouse()
+                      {
+                          
+                          wh_name = row["wh_name"].ToString(),
+                          wh_Shortname = row["wh_Shortname"].ToString()
+                      }).FirstOrDefault();
+            
 
+            return wh;
+        }
         public List<Warehouse> getcontactdetails()
         {
             var user1 = (CustomPrinciple)System.Web.HttpContext.Current.User;
