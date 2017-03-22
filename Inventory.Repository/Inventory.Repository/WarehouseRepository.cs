b03cce62a96584069c373c12a558f357f1ea8134
lokesh <lokeshkumar@xsilica.com>
2017-03-22 15:41:49 +0530

    latest

--- Inventory.Repository/Inventory.Repository/WarehouseRepository.cs
+++ Inventory.Repository/Inventory.Repository/WarehouseRepository.cs
@@ -40,5 +40,90 @@ namespace Inventory.Repository
             ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
             return SqlHelper.ExecuteReader(ConnectionString, "[dbo].[spGetwarehouseDtls]");
         }
+
+        public static SqlDataReader getwhid(string dbname)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            return SqlHelper.ExecuteReader(ConnectionString, "getMaxwhid");
+        }
+
+        public static SqlDataReader getcontactdetail(string dbname, string wh_id)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            return SqlHelper.ExecuteReader(ConnectionString, "getContactBywarehouse", wh_id);
+
+        }
+
+        public static SqlDataReader getlastinsertedwarehouse(string dbname, string wh_id)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            return SqlHelper.ExecuteReader(ConnectionString, "getLastInsertedwarehouse", wh_id);
+        }
+       public static SqlDataReader getallwhdetails(string dbname, string wh_id)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            return SqlHelper.ExecuteReader(ConnectionString, "Getallwhdetailsbywhid", wh_id);
+        }
+        //update warehouse
+
+        public static int updatewarehouse(string dbname, string wh_id,string wh_name,string wh_sname)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "updatewarehouse", wh_id, wh_name, wh_sname);
+            return count;
+        }
+        public static int updatewhaddress(string dbname, string wh_id, string bill_street, string bill_city, string bill_state, string bill_postalcode,
+            string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            int count1 = SqlHelper.ExecuteNonQuery(ConnectionString, "updatewh_address", wh_id, bill_street, bill_city, bill_state, bill_postalcode,
+                bill_country, ship_street, ship_city, ship_state, ship_postalcode, ship_country);
+            return count1;
+        }
+
+        public static int updatewhcontact(string dbname, string wh_id,string Contact_Person,long phone,long Mobile,string Email,string job_position)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "updatewhcontact", wh_id, Contact_Person, phone, Mobile, Email, job_position);
+            return count;
+        }
+        public static int updatewhnotes(string dbname, string wh_id,string Note)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            int count= SqlHelper.ExecuteNonQuery(ConnectionString, "updatewhnotes", wh_id, Note);
+            return count;
+        }
+        public static int insertwhdtls(string dbname, string wh_name,string wh_sname)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "insertwhdtls", wh_name,wh_sname);
+            return count;
+        }
+
+        public static int insertwhaddress(string dbname, string wh_id, string bill_street, string bill_city, string bill_state, string bill_postalcode,
+            string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            int count1 = SqlHelper.ExecuteNonQuery(ConnectionString, "insertwh_address", wh_id, bill_street, bill_city, bill_state, bill_postalcode,
+               bill_country, ship_street, ship_city, ship_state, ship_postalcode, ship_country);
+            return count1;
+        }
+        public static int insertwhcontact(string dbname, string wh_id, string Contact_Person, long phone, long Mobile, string Email, string job_position)
+        {
+            GetConnectionString getConnectionString = new GetConnectionString();
+            ConnectionString = getConnectionString.CustomizeConnectionString(dbname);
+            int count = SqlHelper.ExecuteNonQuery(ConnectionString, "insertwarehouse1", wh_id, Contact_Person, phone, Mobile, Email, job_position);
+            return count;
+        }
     }
 }
--- Inventory.Service/Inventory.Service/WHservice.cs
+++ Inventory.Service/Inventory.Service/WHservice.cs
@@ -16,16 +16,65 @@ namespace Inventory.Service
         }
 
         public static int WHaddressinsert(string WH_Name, string WH_ShortName, string contactperson, string jobposition,
-            long phone, long mobile, string Email, string Note, string bill_street, string bill_city, string bill_state, 
-            string bill_postalcode, string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country,string dbname)
+            long phone, long mobile, string Email, string Note, string bill_street, string bill_city, string bill_state,
+            string bill_postalcode, string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country, string dbname)
         {
-            return WarehouseRepository.WHaddressinsert(WH_Name,WH_ShortName,contactperson,jobposition, 
-                phone, mobile, Email,Note, bill_street, bill_city, bill_state, bill_postalcode, bill_country, 
-                ship_street, ship_city, ship_state, ship_postalcode, ship_country,dbname);
+            return WarehouseRepository.WHaddressinsert(WH_Name, WH_ShortName, contactperson, jobposition,
+                phone, mobile, Email, Note, bill_street, bill_city, bill_state, bill_postalcode, bill_country,
+                ship_street, ship_city, ship_state, ship_postalcode, ship_country, dbname);
         }
         public static SqlDataReader getwarehousedtls(string dbname)
         {
             return WarehouseRepository.getwarehousedtls(dbname);
+        }
+        public static SqlDataReader getwhid(string dbname)
+        {
+            return WarehouseRepository.getwhid(dbname);
+        }
+
+        public static SqlDataReader getcontactdetail(string dbname,string wh_id)
+        {
+            return WarehouseRepository.getcontactdetail(dbname,wh_id);
+        }
+        public static SqlDataReader getlastinsertedwarehouse(string dbname, string wh_id)
+        {
+            return WarehouseRepository.getlastinsertedwarehouse(dbname,wh_id);
+        }
+        public static SqlDataReader getallwhdetails(string dbname,string wh_id)
+        {
+            return WarehouseRepository.getallwhdetails(dbname,wh_id);
+        }
+        public static int updatewarehouse(string dbname, string wh_id, string wh_name, string wh_sname)
+        {
+            return WarehouseRepository.updatewarehouse(dbname, wh_id, wh_name, wh_sname);
+        }
+        public static int updatewhaddress(string dbname, string wh_id, string bill_street, string bill_city, string bill_state, string bill_postalcode,
+            string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country)
+        {
+            return WarehouseRepository.updatewhaddress(dbname, wh_id, bill_street, bill_city, bill_state, bill_postalcode,
+                bill_country, ship_street, ship_city, ship_state, ship_postalcode, ship_country);
+        }
+        public static int updatewhcontact(string dbname, string wh_id, string Contact_Person, long phone, long Mobile, string Email, string job_position)
+        {
+            return WarehouseRepository.updatewhcontact(dbname, wh_id, Contact_Person, phone, Mobile, Email, job_position);
+        }
+        public static int updatewhnote(string dbname, string wh_id,string Note)
+        {
+            return WarehouseRepository.updatewhnotes(dbname, wh_id, Note);
+        }
+        public static int insertwhdls(string dbname, string wh_name, string wh_sname)
+        {
+            return WarehouseRepository.insertwhdtls(dbname, wh_name, wh_sname);
+        }
+        public static int insertwhaddress(string dbname, string wh_id, string bill_street, string bill_city, string bill_state, string bill_postalcode,
+            string bill_country, string ship_street, string ship_city, string ship_state, string ship_postalcode, string ship_country)
+        {
+            return WarehouseRepository.insertwhaddress(dbname, wh_id, bill_street, bill_city, bill_state, bill_postalcode,
+               bill_country, ship_street, ship_city, ship_state, ship_postalcode, ship_country);
+        }
+        public static int insertwhcontact(string dbname, string wh_id, string Contact_Person, long phone, long Mobile, string Email, string job_position)
+        {
+            return WarehouseRepository.insertwhcontact(dbname, wh_id, Contact_Person, phone, Mobile, Email, job_position);
         }
     }
 }
