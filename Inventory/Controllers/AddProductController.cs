a4e7b9a8f4507f7135a83a84c23014c8738fba61
kurarameshsai <rameshsai@xsilica.com>
2017-04-25 19:09:51 +0530

    latest

--- Inventory/Controllers/AddProductController.cs
+++ Inventory/Controllers/AddProductController.cs
@@ -7,6 +7,7 @@ using Inventory.Service;
 using Inventory.Content;
 using System.Data;
 using Inventory.Models;
+using System.Data.SqlClient;
 
 namespace Inventory.Controllers
 {
@@ -29,6 +30,7 @@ namespace Inventory.Controllers
                     ViewBag.categories = convert(user.DbName, "category", "");
                     ViewBag.brands = convert(user.DbName, "brand", "");
                     //ViewBag.brandmodels = convert(user.DbName, "getallmodels");
+                    ViewBag.AvailableWarehouses = WarehouseQuantity(user.DbName);
                 }
                 else
                 {
@@ -44,15 +46,22 @@ namespace Inventory.Controllers
             if (command == "AddProduct")
             {
                 var user = (CustomPrinciple)System.Web.HttpContext.Current.User;
-                int count = ProductService.ProductFunctionalities(command, user.DbName, product.product_name, product.brand, product.model, product.category, product.sub_category,
+                int id = ProductMaxID(user.DbName); // Get Max Product ID
+                string product_id = "P" + id;
+                ViewBag.AvailableWarehouses = WarehouseQuantity(user.DbName);
+                int count = ProductService.ProductFunctionalities(command, user.DbName, id, product_id, product.product_name, product.brand, product.model, product.category, product.sub_category,
                     product.cost_price, product.selling_price, product.tax, product.discount, product.shipping_price, product.total_price, product.Measurement, product.weight,
                     product.size, product.color, product.item_shape, product.product_consumable, product.product_type, product.product_perishability, product.product_expirydate,
                     product.product_description, product.product_tags);
-                if (count > 1)
+                if (count > 0)
                 {
+                    for (int i = 0; i < product.Quantity_Qty.Count; i++)
+                    {
+                        int response = ProductService.AddQuantityInHand(user.DbName, product_id, ViewBag.AvailableWarehouses[i].wh_Shortname, product.Quantity_Qty[i], product.Quantity_Total);
+                    }
                     return Content("<script language='javascript' type='text/javascript'>alert('Product Added Successfully!!!');location.href='" + @Url.Action("Index", "AllProducts") + "'</script>"); // Redirects to AllProducts View
                 }
-                    return Content("<script language='javascript' type='text/javascript'>alert('Failed To Add Product');location.href='" + @Url.Action("Index", "Products") + "'</script>"); // Stays in Same View
+                return Content("<script language='javascript' type='text/javascript'>alert('Failed To Add Product');location.href='" + @Url.Action("Index", "AddProduct") + "'</script>"); // Stays in Same View
             }
             return View();
         }
@@ -126,6 +135,36 @@ namespace Inventory.Controllers
             else
                 return Json("Failed");
             //return Json(JsonRequestBehavior.AllowGet);
+        }
+
+        public int ProductMaxID(string dbname)
+        {
+            var maxid = ProductService.GetProductMaxID(dbname);
+            int id = 0;
+            if (maxid.Read())
+                id = int.Parse(maxid["id"].ToString()) + 1;
+            else
+                id = 1;
+            return id;
+        }
+
+        public List<Warehouse> WarehouseQuantity(string dbname)
+        {
+            SqlDataReader value = WHservice.getwarehousedtls(dbname);
+            DataTable dt = new DataTable();
+            dt.Load(value);
+            List<Warehouse> warehouse = new List<Warehouse>();
+            warehouse = (from DataRow row in dt.Rows
+                         select new Warehouse()
+                         {
+                             //wh_Id = row["wh_id"].ToString(),
+                             wh_name = row["wh_name"].ToString(),
+                             wh_Shortname = row["wh_Shortname"].ToString(),
+                             //conperson = row["Contact_person"].ToString(),
+                             //Email = row["Email"].ToString(),
+                             // Wh_logo = row["Wh_Image"].ToString()
+                         }).OrderByDescending(m => m.wh_Id).ToList();
+            return warehouse;
         }
     }
 }
