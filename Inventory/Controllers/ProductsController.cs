436b36ee2a9a4402a671c5b99a9392cddda54139
sravani <sravani.siddeswara@xsilica.com>
2017-04-26 17:53:26 +0530

    latest

--- Inventory/Controllers/ProductsController.cs
+++ Inventory/Controllers/ProductsController.cs
@@ -15,33 +15,14 @@ namespace Inventory.Controllers
         // GET: Products
         public ActionResult Index()
         {
-            //var list = allProducts();
-            //if (list != null)
-            //    ViewBag.allproducts = allProducts().Select(m => m.product_name).Distinct();
-            //else
-            //    ViewBag.allproducts = "";
-            return View();
+           return View();
         }
         
         public ActionResult purchaseorder()
         {
             return View();
         }
-       //public List<Product> allProducts()
-       // {
-       //     var user = (CustomPrinciple)System.Web.HttpContext.Current.User;
-       //     var records = ProductService.GetAllProducts(user.DbName);
-       //     var dt = new DataTable();
-       //     dt.Load(records);
-       //     List<Product> allproducts = (from DataRow row in dt.Rows select new Product() {
-       //         product_name = row["product_name"].ToString(),
-       //         product_type = row["product_type"].ToString(),
-       //         cost_price = row["cost_price"].ToString(),
-       //         product_images = row["[product_images]"].ToString()
-       // }).Distinct().ToList();
-       //     return allproducts;
-          
-       // }
+       
         public PartialViewResult allproducts()
         {
             if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
