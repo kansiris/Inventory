2cb8740c4662f659a935c09cfd78e9afd5cb20ae
sravani <sravani.siddeswara@xsilica.com>
2017-05-19 13:23:57 +0530

    latest

--- Inventory/Scripts/shopproduct.js
+++ Inventory/Scripts/shopproduct.js
@@ -1,89 +1,10 @@
-﻿$(document).ready(function (e) {
-    
-    var tableprice, divprice, tabletotal, divtotal;
-    $('.spinner .btn:first-of-type').click(function () {
-        var plus, v;
-        v = parseInt($(this).parent("div").prev("input[type='text']").val());
-        //alert(v);
-        if (v > 0) {
-            plus = v + 1;
-            $(this).parent("div").prev("input[type='text']").val(plus);
-            tableprice = parseInt($(this).parents(".quantity-changes").nextAll(".product-price").children("span").text());
-            divprice = parseInt($(this).parents(".rate-add").children(".product-price").children("span").text());
-
-            tabletotal = plus * tableprice;
-            divtotal = plus * divprice;
-            $(this).parents(".quantity-changes").nextAll(".product-price-total").children("span").remove();
-            $(this).parents(".quantity-changes").nextAll(".product-price-total").append("<span>" + tabletotal + "</span>");
-            $(this).parents(".rate-add").children(".product-price-total").children("span").remove();
-            $(this).parents(".rate-add").children(".product-price-total").append("<span>" + divtotal + "</span>");
-        }
-        return vals();
-    });
-    $('.spinner .btn:last-of-type').click(function () {
-        var minus, v;
-        v = parseInt($(this).parent("div").prev("input[type='text']").val());
-
-        if (v > 1) {
-            minus = v - 1;
-            $(this).parent("div").prev("input[type='text']").val(minus);
-            tableprice = parseInt($(this).parents(".quantity-changes").nextAll(".product-price").children("span").text());;
-            divprice = parseInt($(this).parents(".rate-add").children(".product-price").children("span").text());
-            tabletotal = minus * tableprice;
-            divtotal = minus * divprice; $(this).parents(".quantity-changes").nextAll(".product-price-total").children("span").remove();
-            $(this).parents(".quantity-changes").nextAll(".product-price-total").append("<span>" + tabletotal + "</span>");
-            $(this).parents(".rate-add").children(".product-price-total").children("span").remove();
-            $(this).parents(".rate-add").children(".product-price-total").append("<span>" + divtotal + "</span>");
-        }
-        return vals();
-    });
-
-    $("select").on('change', function () {
-        var v = $(this).children("option:selected").text().split("₹")[1];
-        $(this).parents(".product-variation").nextAll(".rate-add").children(".product-price").empty().append("Price ₹ " + "<span>" + v + "</span>");
-        $(this).parents(".product-variation").nextAll(".rate-add").children(".product-price-total").empty().append("Total ₹ " + "<span>" + v + "</span>");
-        $(this).parents(".product-variation").nextAll(".product-price").empty().append("₹ " + "<span>" + v + "</span>");
-        $(this).parents(".product-variation").nextAll(".product-price-total").empty().append("₹ " + "<span>" + v + "</span>");
-        $(this).closest('div.row').find("div.qty").children("div.spinner").children("input").val('1');
-
-    });
-});
-
-function vals() {
-  
-    var total, totalquantity = 0, productprice, carttotal = 0;
-    $(".added-products .spinner input[type='text']").each(function () {
-        total = parseInt($(this).val());
-        totalquantity = total + totalquantity;
-    });
-    $(".added-products .product-price-total span").each(function () {
-        productprice = parseInt($(this).text());
-        carttotal = productprice + carttotal;
-    });
-    $(".cart-total span").remove();
-    $(".cart-total").append("<span>" + carttotal + "</span>");
-    $(".cart-icn span").remove();
-    $(".cart-icn").append("<span>" + totalquantity + "</span>");
-}
-
-$(".add-to-cart-table > .btn, .add-to-cart > .btn").click(function () {
-    return vals();
-    
-});
-
-$(".remove").click(function () {
-    $(this).parents("div.well,tr").remove();
-    return vals();
-
-});
-
+﻿
 function ajaxcalling(clickeditem) {
-    //alert(clickeditem);
     var cid = document.URL.split('=')[1].split('#')[0];
     $.ajax({
         url: '/Products/Getproducts',
         type: 'POST',
-        data: JSON.stringify({ sub_category: clickeditem}),
+        data: JSON.stringify({ sub_category: clickeditem }),
         dataType: 'json',
         contentType: 'application/json',
         success: function (data) {
@@ -94,9 +15,6 @@ function ajaxcalling(clickeditem) {
             else {
                 var url1 = 'Products/Getproductsbysubcategory?sub_category=' + data;
                 $('#allproducts').empty.load(url1);
-                //var url2 = 'Products/Addtocartpartial?cid='+cid;
-                //$('#cartrecords').empty.load(url2);
-               
             }
         },
         error: function (data)
@@ -106,8 +24,7 @@ function ajaxcalling(clickeditem) {
 
 
 function getproducts(value) {
-    // ajaxcalling(value);
-    
+
     var cid = document.URL.split('=')[1].split('#')[0];
     $.ajax({
         url: '/Products/Getproducts',
@@ -123,9 +40,6 @@ function getproducts(value) {
             else {
                 var url = 'Products/Getproductsbysubcategory?sub_category=' + data;
                 $('#allproducts').load(url);
-                //var url2 = 'Products/Addtocartpartial?cid='+cid;
-                //$('#cartrecords').empty.load(url2);
-                
             }
         },
         error: function (data)
@@ -138,7 +52,7 @@ function getproducts(value) {
 //removing from cart
 function removecart(cartid) {
     var cid = location.search.split('cid=')[1];
-    //alert(cid);
+
     $.ajax({
         url: '/Products/Removefromcart?cart_id=' + cartid,
         type: 'POST',
@@ -151,9 +65,8 @@ function removecart(cartid) {
             }
             else {
                 alert("Removed from cart Successfully .");
-                var url = 'Products/Addtocartpartial?cid='+cid;
+                var url = 'Products/Addtocartpartial?cid=' + cid;
                 $('#cartrecords').empty.load(url);
-                
             }
         },
         error: function (data)
@@ -166,22 +79,15 @@ function removecart(cartid) {
 
 function genaratepo() {
     var customerid = document.URL.split('=')[1];
-    //alert(customerid);
-    location.href='/Products/GenaratePOs?cid='+customerid;
-    
+    location.href = '/Products/GenaratePOs?cid=' + customerid;
+
 }
 
 //updatecart
 function updatecart(cartid, quantity, costprice) {
-    //alert(cartid);
-    //alert(quantity);
-    //alert(costprice);
     cid = location.search.split('cid=')[1];
     newquant = quantity + 1;
     newamunt = costprice * newquant;
-    //alert(cid);
-    //alert("new" + newamunt);
-
     $.ajax({
         url: '/Products/UpdateCart',
         type: 'POST',
@@ -191,12 +97,11 @@ function updatecart(cartid, quantity, costprice) {
         success: function (data) {
             if (data == "unique") {
                 alert("not inserted");
-            } 
+            }
             else {
                 alert("Successfully Updated Cart.");
                 var url = 'Products/Addtocartpartial?cid=' + cid;
-                $('#cartrecords').empty.load(url);
-               
+                $('#cartrecords').load(url);
             }
         },
         error: function (data)
@@ -205,15 +110,10 @@ function updatecart(cartid, quantity, costprice) {
 
 }
 function updatecart1(cartid, quantity, costprice) {
-    //alert(cartid);
-    //alert(quantity);
-    //alert(costprice);
+
     newquant = quantity - 1;
     newamunt = costprice * newquant;
     cid = location.search.split('cid=')[1];
-    //alert(cid);
-    //alert("new" + newamunt);
-
     $.ajax({
         url: '/Products/UpdateCart',
         type: 'POST',
@@ -226,14 +126,14 @@ function updatecart1(cartid, quantity, costprice) {
             }
             else {
                 alert("Successfully Updated Cart.");
-                var url = 'Products/Addtocartpartial?cid='+cid;
-                $('#cartrecords').empty.load(url);
-                //location.reload();
+                var url = 'Products/Addtocartpartial?cid=' + cid;
+                $('#cartrecords').load(url);
             }
         },
         error: function (data)
         { alert("Failed!!!"); }
     });
-
 }
+
+
 
