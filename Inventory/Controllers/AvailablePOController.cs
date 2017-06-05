5c1cee88d6d7945288d44fc15477a0da34307074
kurarameshsai <rameshsai@xsilica.com>
2017-06-05 11:42:18 +0530

    latest

---
+++ Inventory/Controllers/AvailablePOController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory.Controllers
+{
+    public class AvailablePOController : Controller
+    {
+        // GET: AvailablePO
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
