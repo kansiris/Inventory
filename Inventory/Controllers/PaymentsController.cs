e74f41f55accca00fe241d85a3de74ca9c155aee
sravani <sravani.siddeswara@xsilica.com>
2017-06-28 15:43:04 +0530

    latest

--- Inventory/Controllers/PaymentsController.cs
+++ Inventory/Controllers/PaymentsController.cs
@@ -19,9 +19,31 @@ namespace Inventory.Controllers
 {
     public class PaymentsController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string cid, string cname)
         {
+            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                var user = (CustomPrinciple)System.Web.HttpContext.Current.User;
+                var dt = new DataTable();
+                var records = PaymentsService.InvoicesForPayment(user.DbName, cid);
+                dt.Load(records);
+                List<Invoice> availableinvoices = (from DataRow row in dt.Rows
+                                                   select new Invoice()
+                                                   {
+                                                       customer_id = row["customer_id"].ToString(),
+                                                       company_name = row["company_name"].ToString(),
+                                                       Prchaseorder_no = row["Prchaseorder_no"].ToString(),
+                                                       total_price = row["total_price"].ToString(),
+                                                       created_date = row["created_date"].ToString(),
+                                                       invoice_status = row["invoice_status"].ToString(),
+                                                       totalQty = row["totalQty"].ToString()
+                                                   }).OrderByDescending(m => m.invoice_status).ToList();
+                ViewBag.records = availableinvoices;
+                ViewBag.invoice_status = availableinvoices.Select(m => m.invoice_status);
+                ViewBag.total_price = availableinvoices.Select(m => float.Parse(m.total_price)).Sum();
+            }
             return View();
         }
+
     }
 }
